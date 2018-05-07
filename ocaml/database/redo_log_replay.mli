fb4774a1ec7b2bc6b23118214dd54b89325f8605
Edwin Török <edvin.torok@citrix.com>
2018-05-07 09:12:15 +0000

    CA-288380: redo log testing: move redo log replay to database/
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- ocaml/database/redo_log_replay.mli
+++ ocaml/database/redo_log_replay.mli
@@ -11,14 +11,10 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-module R = Debug.Make(struct let name = "redo_log" end)
+(**
+ * @group Redo-log
+*)
 
-let read_from_redo_log log staging_path db_ref =
-  Redo_log_replay.replay_from_redo_log log staging_path (Datamodel_schema.of_datamodel ()) db_ref
-
-let stop_using_redo_log log =
-  R.debug "Stopping using redo log";
-  try
-    Redo_log.shutdown log
-  with _ -> () (* best effort only *)
-
+(** Connect to the block device and write the latest version of the database
+ * on it to a file with a given name. *)
+val replay_from_redo_log : Redo_log.redo_log -> string -> Schema.t -> Db_ref.t -> unit
