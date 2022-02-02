82d5e840ffa247fe897d0529a69187e8753e6ae6
Edwin Török <edvin.torok@citrix.com>
2022-02-02 21:00:01 +0000

    Wip
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

---
+++ ocaml/opentelemetry/context.ml
@@ -0,0 +1,13 @@
+type t = {map: Hmap.t; active: Span.t option}
+
+type 'a key = 'a Hmap.key * string
+
+let create_key name : 'a key = (Hmap.Key.create (), name)
+
+let get_value t (key, _) = Hmap.find key t.map
+
+let set_value t (key, _) v = {t with map= Hmap.add key v t.map}
+
+let get_active t = t.active
+
+let set_active t active = {t with active}
