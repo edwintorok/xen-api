44167ec880a54d45e0edf0cfec2cb293afb7e36e
Edwin Török <edvin.torok@citrix.com>
2022-04-04 15:48:06 +0100

    CA-364138: destroy varstored-guard connection even if chroot got cleaned up already
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- ocaml/xenopsd/lib/xenops_sandbox.ml
+++ ocaml/xenopsd/lib/xenops_sandbox.ml
@@ -145,18 +145,17 @@ module Varstore_guard = struct
 
   let stop dbg ~domid ~vm_uuid =
     let chroot = varstored_chroot ~domid ~vm_uuid in
-    if Sys.file_exists chroot.root then (
-      D.debug "About to stop varstored for %d (%s) %s" domid vm_uuid __LOC__ ;
-      let gid = chroot.Chroot.gid in
-      let absolute_socket_path =
-        Chroot.absolute_path_outside chroot socket_path
-      in
-      Xenops_utils.best_effort "Stop listening on deprivileged socket"
-        (fun () ->
-          Varstore_privileged_client.Client.destroy dbg gid absolute_socket_path
-      ) ;
+    D.debug "About to stop varstored for %d (%s) %s" domid vm_uuid __LOC__ ;
+    let gid = chroot.Chroot.gid in
+    let absolute_socket_path =
+      Chroot.absolute_path_outside chroot socket_path
+    in
+    Xenops_utils.best_effort "Stop listening on deprivileged socket" (fun () ->
+        Varstore_privileged_client.Client.destroy dbg gid absolute_socket_path
+    ) ;
+    if Sys.file_exists chroot.root then
       Chroot.destroy chroot
-    ) else
+    else
       D.warn "Can't stop varstored for %d (%s): %s does not exist" domid vm_uuid
         chroot.root
 end
