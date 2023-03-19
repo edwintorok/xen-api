84d096672d8ed09e639fe87efb89f8f7777fae45
Edwin Török <edwin.torok@cloud.com>
2023-03-19 19:13:25 +0000

    [maintenance]: reduce basic-rpc-test time
    Remove most sleeps, and reduce test duration from 5 seconds to 1.
    (If we do want to run a performance test we can increase these again)
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/message-switch/core_test/basic-rpc-test.sh
+++ ocaml/message-switch/core_test/basic-rpc-test.sh
@@ -11,28 +11,22 @@ echo Checking the switch can start late
 ./server_unix_main.exe -path $SPATH &
 sleep 1
 ../switch/switch_main.exe --path $SPATH --statedir ${SWITCHPATH} &
-./client_unix_main.exe -path $SPATH -secs 5
-sleep 2
+./client_unix_main.exe -path $SPATH -secs 1
 
 echo Performance test of Unix to Unix
 ./server_unix_main.exe -path $SPATH &
-./client_unix_main.exe -path $SPATH -secs 5
-sleep 2
+./client_unix_main.exe -path $SPATH -secs 1
 
 echo Performance test of Lwt to Lwt
 lwt/server_main.exe -path $SPATH &
-lwt/client_main.exe -path $SPATH -secs 5
-sleep 2
+lwt/client_main.exe -path $SPATH -secs 1
 
 echo Performance test of Async to Lwt
 lwt/server_main.exe -path $SPATH &
-async/client_async_main.exe -path $SPATH -secs 5
-sleep 2
+async/client_async_main.exe -path $SPATH -secs 1
 
 echo Performance test of Async to Async
 async/server_async_main.exe -path $SPATH &
-async/client_async_main.exe -path $SPATH -secs 5
-sleep 2
+async/client_async_main.exe -path $SPATH -secs 1
 
 ../cli/main.exe shutdown --path $SPATH
-sleep 2
