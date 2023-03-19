c34467baae675b1df1614885eb983606f7b2c76e
Edwin Török <edwin.torok@cloud.com>
2023-03-19 16:38:14 +0000

    [maintenance]: remove dependency between most tests and server.ml
    Server.ml takes a while to compile, but most unit tests don't actually need it.
    Reorganize Api_server into Api_server+Api_server_common, where the latter
    suffices for unit tests.
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/tests/common/test_common.ml
+++ ocaml/tests/common/test_common.ml
@@ -494,7 +494,7 @@ let make_pool_update ~__context ?(ref = Ref.make ()) ?(uuid = make_uuid ())
       ; after_apply_guidance
       ; enforce_homogeneity
       }
-    
+
   in
 
   Xapi_pool_update.create_update_record ~__context ~update:ref ~update_info ~vdi ;
@@ -512,27 +512,6 @@ let make_session ~__context ?(ref = Ref.make ()) ?(uuid = make_uuid ())
     ~auth_user_sid ~auth_user_name ~rbac_permissions ~parent ~originator
     ~client_certificate ;
   ref
-
-(** Returns a [(rpc, session_id)] pair that can be passed to the
-    functions within the [Client] module to make XenAPI calls. The
-    calls can only succeed if they get forwarded to the local host
-    by the message forwarding layer. Forwarding to slaves does not
-    work in unit tests. *)
-let make_client_params ~__context =
-  let req = Xmlrpc_client.xmlrpc ~version:"1.1" "/" in
-  let rpc = Api_server.Server.dispatch_call req Unix.stdout in
-  let session_id =
-    let session_id = Ref.make () in
-    let now = Xapi_stdext_date.Date.of_float (Unix.time ()) in
-    let (_ : _ API.Ref.t) =
-      make_session ~__context ~ref:session_id
-        ~this_host:(Helpers.get_localhost ~__context)
-        ~last_active:now ~is_local_superuser:true ~validation_time:now
-        ~auth_user_name:"root" ~originator:"test" ()
-    in
-    session_id
-  in
-  (rpc, session_id)
 
 let create_physical_pif ~__context ~host ?network ?(bridge = "xapi0")
     ?(managed = true) () =
