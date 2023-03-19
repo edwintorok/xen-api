4bc1e729d00517b156a409e3dcad12038983c859
Edwin Török <edwin.torok@cloud.com>
2023-03-19 19:29:39 +0000

    [maintenance]: reduce basic-rpc-test.sh testing time
    Run just a basic test for 0.1 seconds instead of a performance test for 5s by
    default.
    (can still be tweaked by overriding SECS)
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/message-switch/core_test/basic-rpc-test.sh
+++ ocaml/message-switch/core_test/basic-rpc-test.sh
@@ -3,30 +3,35 @@ set -e
 
 SPATH=${TMPDIR:-/tmp}/sock
 SWITCHPATH=${TMPDIR:-/tmp}/switch
+SECS=0.1
 
-
-rm -rf ${SWITCHPATH} && mkdir -p ${SWITCHPATH}
+rm -rf "${SWITCHPATH}" && mkdir -p "${SWITCHPATH}"
 
 echo Checking the switch can start late
-./server_unix_main.exe -path $SPATH &
-sleep 1
-../switch/switch_main.exe --path $SPATH --statedir ${SWITCHPATH} &
-./client_unix_main.exe -path $SPATH -secs 1
+./server_unix_main.exe -path "${SPATH}" &
+sleep "${SECS}"
+../switch/switch_main.exe --path "${SPATH}" --statedir "${SWITCHPATH}" &
+./client_unix_main.exe -path "${SPATH}" -secs "${SECS}"
+wait -n
 
 echo Performance test of Unix to Unix
-./server_unix_main.exe -path $SPATH &
-./client_unix_main.exe -path $SPATH -secs 1
+./server_unix_main.exe -path "${SPATH}" &
+./client_unix_main.exe -path "${SPATH}" -secs "${SECS}"
+wait -n
 
 echo Performance test of Lwt to Lwt
-lwt/server_main.exe -path $SPATH &
-lwt/client_main.exe -path $SPATH -secs 1
+lwt/server_main.exe -path "${SPATH}" &
+lwt/client_main.exe -path "${SPATH}" -secs "${SECS}"
+wait -n
 
 echo Performance test of Async to Lwt
-lwt/server_main.exe -path $SPATH &
-async/client_async_main.exe -path $SPATH -secs 1
+lwt/server_main.exe -path "${SPATH}" &
+async/client_async_main.exe -path "${SPATH}" -secs "${SECS}"
+wait -n
 
 echo Performance test of Async to Async
-async/server_async_main.exe -path $SPATH &
-async/client_async_main.exe -path $SPATH -secs 1
+async/server_async_main.exe -path "${SPATH}" &
+async/client_async_main.exe -path "${SPATH}" -secs "${SECS}"
+wait -n
 
-../cli/main.exe shutdown --path $SPATH
+../cli/main.exe shutdown --path "${SPATH}"
