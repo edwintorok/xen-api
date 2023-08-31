edwintorok/xen-api

commit 6a7cf5bb65f647485c28b266ff79d6fb056d1a94

CA-378229: flush database immediately on redo log enable
The redo log protects the XAPI database against (host) crashes.
When the redo log is first enabled there might be some pending data in the XAPI database (in memory),
but this will only be written out when the database is next written to (which could be minutes later).

Perform an immediate database flush when we enable the redo log and force all callers to decide whether they need this
behaviour or not. Code that wants to recover or read back a redo log must not flush the database,
but code that enables the redo log with an already active database must flush it.

This should ensure that when the 'enable_redo_log' API completes we have a crash-proof XAPI database from that point forward.

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/database/redo_log.ml
@@ -888,6 +888,26 @@ let flush_db_to_redo_log db log =
     write_db_to_fd log ;
   !(log.currently_accessible)
 
+let flush_db_exn db log =
+  match (db, log.read_only) with
+  | None, true ->
+      () (* no-op *)
+  | Some db, false ->
+      R.debug "Flushing database on redo log enable" ;
+      if not (flush_db_to_redo_log db log) then
+        raise (RedoLogFailure "Cannot connect to redo log")
+  | None, false ->
+      (* can happen during startup when we want to replay redo log *)
+      R.debug "No database provided, but redo-log is not readonly"
+  | Some _, true ->
+      invalid_arg
+        "Database provided during Redo_log.enable, but the redo log is \
+         read-only!"
+
+let enable db log reason = enable log reason ; flush_db_exn db log
+
+let enable_block db log path = enable_block log path ; flush_db_exn db log
+
 (* Write the given database to all active redo_logs *)
 let flush_db_to_all_active_redo_logs db =
   R.info "Flushing database to all active redo-logs" ;

ocaml/database/redo_log.mli
@@ -49,10 +49,11 @@ type redo_log = {
 val is_enabled : redo_log -> bool
 (** Returns [true] iff writing deltas to the block device is enabled. *)
 
-val enable : redo_log -> string -> unit
+val enable : Db_cache_types.Database.t option -> redo_log -> string -> unit
 (** Enables writing deltas to the block device. Subsequent modifications to the database will be persisted to the block device. Takes a static-VDI reason as argument to select the device to use. *)
 
-val enable_block : redo_log -> string -> unit
+val enable_block :
+  Db_cache_types.Database.t option -> redo_log -> string -> unit
 (** Enables writing deltas to the block device. Subsequent modifications to the database will be persisted to the block device. Takes a path as argument to select the device to use. *)
 
 val disable : redo_log -> unit

ocaml/xapi/xapi.ml
@@ -486,7 +486,8 @@ let start_redo_log () =
     then (
       debug "Redo log was enabled when shutting down, so restarting it" ;
       (* enable the use of the redo log *)
-      Redo_log.enable Xapi_ha.ha_redo_log Xapi_globs.gen_metadata_vdi_reason ;
+      Redo_log.enable None Xapi_ha.ha_redo_log
+        Xapi_globs.gen_metadata_vdi_reason ;
       debug "Attempting to extract a database from a metadata VDI" ;
       (* read from redo log and store results in a staging file for use in the
        * next step; best effort only: does not raise any exceptions *)

ocaml/xapi/xapi_ha.ml
@@ -926,7 +926,9 @@ let redo_log_ha_enabled_during_runtime __context =
     Redo_log.switch ha_redo_log Xapi_globs.ha_metadata_vdi_reason
   ) else (
     info "Switching on HA redo log." ;
-    Redo_log.enable ha_redo_log Xapi_globs.ha_metadata_vdi_reason
+    Redo_log.enable
+      (Some (Context.database_of __context |> Db_ref.get_database))
+      ha_redo_log Xapi_globs.ha_metadata_vdi_reason
     (* upon the first attempt to write a delta, it will realise that a DB flush
      * is necessary as the I/O process will not be running *)
   )
@@ -952,7 +954,7 @@ let redo_log_ha_enabled_at_startup () =
   (* If we are still the master, extract any HA metadata database so we can consider population from it *)
   if Pool_role.is_master () then (
     debug "HA is enabled, so enabling writing to redo-log" ;
-    Redo_log.enable ha_redo_log Xapi_globs.ha_metadata_vdi_reason ;
+    Redo_log.enable None ha_redo_log Xapi_globs.ha_metadata_vdi_reason ;
     (* enable the use of the redo log *)
     debug
       "This node is a master; attempting to extract a database from a metadata \

ocaml/xapi/xapi_pool.ml
@@ -2924,7 +2924,9 @@ let enable_redo_log ~__context ~sr =
   (* enable the new redo log, unless HA is enabled (which means a redo log
      	 * is already in use) *)
   if not (Db.Pool.get_ha_enabled ~__context ~self:pool) then (
-    Redo_log.enable Xapi_ha.ha_redo_log Xapi_globs.gen_metadata_vdi_reason ;
+    Redo_log.enable
+      (Some (Context.database_of __context |> Db_ref.get_database))
+      Xapi_ha.ha_redo_log Xapi_globs.gen_metadata_vdi_reason ;
     Localdb.put Constants.redo_log_enabled "true"
   ) ;
   info "The redo log is now enabled"

ocaml/xapi/xapi_vdi_helpers.ml
@@ -145,7 +145,9 @@ let enable_database_replication ~__context ~get_vdi_callback =
         in
         let device = Db.VBD.get_device ~__context ~self:vbd in
         try
-          Redo_log.enable_block log ("/dev/" ^ device) ;
+          Redo_log.enable_block
+            (Some (Context.database_of __context |> Db_ref.get_database))
+            log ("/dev/" ^ device) ;
           Hashtbl.add metadata_replication vdi (vbd, log) ;
           let vbd_uuid = Db.VBD.get_uuid ~__context ~self:vbd in
           Db.VDI.set_metadata_latest ~__context ~self:vdi ~value:true ;
@@ -200,7 +202,7 @@ let database_ref_of_vdi ~__context ~vdi =
         ~state_change_callback:None ~read_only:true
     in
     debug "Enabling redo_log with device reason [%s]" device ;
-    Redo_log.enable_block log device ;
+    Redo_log.enable_block None log device ;
     let db = Database.make (Datamodel_schema.of_datamodel ()) in
     let db_ref = Db_ref.in_memory (ref (ref db)) in
     Redo_log_usage.read_from_redo_log log Xapi_globs.foreign_metadata_db db_ref ;

commit 10ac29e0adc740a02853bb8482faec622d6304a8

protect Redo_log.enable with a mutex
Fixes: 6a7cf5bb6 ("CA-378229: flush database immediately on redo log enable")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/database/redo_log.ml
@@ -886,9 +886,11 @@ let flush_db_exn db log =
     raise (RedoLogFailure "Cannot connect to redo log")
 
 let enable_and_flush db log reason =
+  with_lock redo_log_creation_mutex @@ fun () ->
   enable_existing log reason ; flush_db_exn db log
 
 let enable_block_and_flush db log path =
+  with_lock redo_log_creation_mutex @@ fun () ->
   enable_block_existing log path ;
   flush_db_exn db log
 