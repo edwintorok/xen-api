b1d0a6e27d898d29d718219cfef68ef7b5a2031f
Edwin Török <edwin.torok@cloud.com>
2024-02-09 11:27:08 +0000

    add Map and Set type combinators for RPC
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/xapi-idl/lib/typeCombinators.ml
+++ ocaml/xapi-idl/lib/typeCombinators.ml
@@ -45,3 +45,99 @@ let triple ?name ?(description = []) (p1, p2, p3) =
       ~some:Fun.id name
   in
   {name; description; ty= Tuple3 (p1.ty, p2.ty, p3.ty)}
+
+let abstract_def ?name ?(description = []) ?test_data of_other to_other def_of_other =
+  let open Rpc.Types in
+  let name =
+    Option.value name
+      ~default:(Printf.sprintf "abstract type based on %s" def_of_other.name)
+  in
+  let typ = def_of_other.ty in
+  let test_data = match test_data with
+    | Some x -> x
+    | None ->
+        Rpc_genfake.gentest typ |> List.rev_map of_other
+  in
+  let typ_of =
+    let rpc_of t = t |> to_other |> Rpcmarshal.marshal typ in
+    let of_rpc rpc = rpc |> Rpcmarshal.unmarshal typ |> Result.map of_other in
+    Rpc.Types.Abstract
+      {
+        aname= name
+      ; test_data
+      ; rpc_of
+      ; of_rpc
+      }
+  in
+  {name; description; ty= typ_of}
+
+let abstract ?name ?(description = []) ?test_data of_other to_other def_of_other
+    sexp_of_other other_of_sexp =
+  let t = abstract_def ?name ~description ?test_data of_other to_other def_of_other in
+  let t_of_sexp sexp = sexp |> other_of_sexp |> of_other in
+
+  let sexp_of_t t = t |> to_other |> sexp_of_other in
+  (t, sexp_of_t, t_of_sexp)
+
+module type Convertable = sig
+  type t
+
+  val typ_of : t Rpc.Types.typ
+  (** [typ_of] is the {!type:Rpc.Types.typ} description on how to convert {!type:t} to and from {!type:Rpc.t} *)
+
+  val t : t Rpc.Types.def
+  (** [t] is the {!type:Rpc.Types.def} description of a {!type:t} convertible to {!type:Rpc.t} *)
+
+  val sexp_of_t : t -> Sexplib.Sexp.t
+  (** [sexp_of_t t] converts [t] to {!type:Sexplib.Sexp.t} *)
+
+  val t_of_sexp : Sexplib.Sexp.t -> t
+  (** [t_of_sexp sexp] converts [sexp] to {!type t} *)
+end
+
+module type OrderedType = sig
+  include Map.OrderedType
+
+  include Convertable with type t := t
+end
+
+open Sexplib.Std
+
+module MakeMap (Ord : OrderedType) = struct
+  include Map.Make (Ord)
+
+  type 'a other = (Ord.t * 'a) list [@@deriving rpcty, sexp]
+
+  let of_other other = other |> List.to_seq |> of_seq
+
+  let to_other = bindings
+
+  let t ?name ?description t_of_elt =
+    let open Rpc.Types in
+    let name =
+      Option.value name
+        ~default:(Printf.sprintf "map from %s to %s" Ord.t.name t_of_elt.name)
+    in
+    abstract_def ~name ?description of_other to_other (other t_of_elt.ty)
+
+  let t_of_sexp elt_of_sexp sexp = sexp |> other_of_sexp elt_of_sexp |> of_other
+
+  let sexp_of_t sexp_of_elt t = t |> to_other |> sexp_of_other sexp_of_elt
+end
+
+module MakeSet (Ord : OrderedType) = struct
+  include Set.Make (Ord)
+
+  type other = Ord.t list [@@deriving rpcty, sexp]
+
+  let of_other = of_list
+
+  let to_other = elements
+
+  let t, sexp_of_t, t_of_sexp =
+    let open Rpc.Types in
+    let name = Printf.sprintf "set of %s" Ord.t.name in
+    abstract ~name of_other to_other other sexp_of_other other_of_sexp
+
+  let typ_of = t.ty
+end
--- ocaml/xapi-idl/lib/typeCombinators.mli
+++ ocaml/xapi-idl/lib/typeCombinators.mli
@@ -41,3 +41,61 @@ val triple :
   -> ?description:string list
   -> 'a Rpc.Types.def * 'b Rpc.Types.def * 'c Rpc.Types.def
   -> ('a * 'b * 'c) Rpc.Types.def
+
+val abstract :
+     ?name:string
+  -> ?description:string list
+  -> ?test_data: 'b list
+  -> ('a -> 'b)
+  -> ('b -> 'a)
+  -> 'a Rpc.Types.def
+  -> ('a -> Sexplib.Sexp.t)
+  -> (Sexplib.Sexp.t -> 'a)
+  -> 'b Rpc.Types.def * ('b -> Sexplib.Sexp.t) * (Sexplib.Sexp.t -> 'b)
+(** [abstract ?name ?description of_other to_other typ_of_other sexp_of_other other_of_sexp] are serialization and deserialization functions
+  for an abstract type given conversion functions and serialization/deserialization functions for another type. *)
+
+module type Convertable = sig
+  type t
+
+  val typ_of : t Rpc.Types.typ
+  (** [typ_of] is the {!type:Rpc.Types.typ} description on how to convert {!type:t} to and from {!type:Rpc.t} *)
+
+  val t : t Rpc.Types.def
+  (** [t] is the {!type:Rpc.Types.def} description of a {!type:t} convertible to {!type:Rpc.t} *)
+
+  val sexp_of_t : t -> Sexplib.Sexp.t
+  (** [sexp_of_t t] converts [t] to {!type:Sexplib.Sexp.t} *)
+
+  val t_of_sexp : Sexplib.Sexp.t -> t
+  (** [t_of_sexp sexp] converts [sexp] to {!type t} *)
+end
+
+module type OrderedType = sig
+  include Map.OrderedType
+
+  include Convertable with type t := t
+end
+
+module MakeMap (Ord : OrderedType) : sig
+  include Map.S with type key = Ord.t
+
+  val t :
+       ?name:string
+    -> ?description:string list
+    -> 'a Rpc.Types.def
+    -> 'a t Rpc.Types.def
+  (** [t ?name ?description element] is the {!type:Rpc.Types.def} for a map with {!type:Ord.t} keys and [element] values. *)
+
+  val sexp_of_t : ('a -> Sexplib.Sexp.t) -> 'a t -> Sexplib.Sexp.t
+  (** [sexp_of_t sexp_of_element t] converts a [t] map to {!type:Sexplib.Sexp.t} using [sexp_of_element] to convert values. *)
+
+  val t_of_sexp : (Sexplib.Sexp.t -> 'a) -> Sexplib.Sexp.t -> 'a t
+  (** [t_of_sexp element_of_sexp sexp] converts a [sexp] to a {!type t} map using [element_of_sexp] to convert values. *)
+end
+
+module MakeSet (Ord : OrderedType) : sig
+  include Set.S with type elt = Ord.t
+
+  include Convertable with type t := t
+end
