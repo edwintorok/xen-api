6f0e6bb6d1017de9af89249a9c2609ae938ed927
Edwin Török <edwin.torok@cloud.com>
2024-04-12 15:05:04 +0100

    propagate W3C traceparent
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/sdk-gen/csharp/autogen/src/JsonRpc.cs
+++ ocaml/sdk-gen/csharp/autogen/src/JsonRpc.cs
@@ -316,6 +316,21 @@ namespace XenAPI
                     webRequest.Headers.Add(header.Key, header.Value);
             }
 
+            // propagate W3C traceparent and tracestate
+            // HttpClient would do this automatically on .NET 5,
+            // and .NET 6 would provide even more control over this: https://blog.ladeak.net/posts/opentelemetry-net6-httpclient
+            // the caller must ensure that the activity is in W3C format (by inheritance or direc setting)
+            var activity = Activity.Current;
+            if (activity != null && activity.IdFormat == ActivityIdFormat.W3C)
+            {
+                webRequest.Headers.Add("traceparent", activity.Id);
+                var state = activity.TraceStateString;
+                if (state?.Length > 0)
+                {
+                    webRequest.Headers.Add("tracestate", state);
+                }
+            }
+
             using (var str = webRequest.GetRequestStream())
             {
                 postStream.CopyTo(str);
