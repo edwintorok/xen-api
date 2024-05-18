a3b1c3c94bd03f9272dc73b297630e5e99c60c74
Edwin Török <edwin.torok@cloud.com>
2024-05-18 20:13:03 +0100

    wip

--- latency/unixlatency.ml
+++ latency/unixlatency.ml
@@ -1,64 +1,73 @@
-let significant_figures = 3
-
 let init () =
-  Hdr_histogram.init ~lowest_discernible_value:1
-    ~highest_trackable_value:1_000_000 ~significant_figures
+  Hdr_histogram.init ~lowest_discernible_value:32
+    ~highest_trackable_value:500_000_000_000 ~significant_figures:3
 
 (** doesn't allocate *)
 let mtime () = Monotonic_clock.now () |> Int64.to_int
 
-let rec measure_self hdr n t0 t1 =
-  if n > 0 then (
-    let ok = Hdr_histogram.record_value hdr (t1 - t0) in
-    assert ok ;
-    let t2 = mtime () in
-    measure_self hdr (n - 1) t1 t2 )
-
-let percentiles = [50.; 95.; 99.; 99.9; 100.]
-
-let dump_stats hdr =
-  Hdr_histogram.hdr_percentiles_print hdr "/dev/stdout" 5l 1e3 Hdr_histogram.CLASSIC
-
-let measure_self m n =
-  let hdr = init () in
-  for _ = 1 to m do
-    let t0 = mtime () in
-    let t1 = mtime () in
-    measure_self hdr n t0 t1 ;
-    (* avoid being interrupted while measuring, go back to the OS from time to time *)
-    Unix.sleepf 0.001
-  done;
-  dump_stats hdr ;
-  Hdr_histogram.close hdr
-
-let hdr = init ()
-
-let measure socket addr () =
+let measure hdr f arg1 =
   let t0 = mtime () in
-  Unix.connect socket addr ;
+  let r = f arg1 in
   let t1 = mtime () in
-  let d = t1 - t0 in
-  let ok = Hdr_histogram.record_value hdr d in
-  assert ok
+  let ok = Hdr_histogram.record_value hdr (t1 - t0) in
+  assert ok;
+  r
+
+let dump_stats name hdr =
+  Hdr_histogram.hdr_percentiles_print hdr name 5l 1e3 Hdr_histogram.CLASSIC;
+  Printf.eprintf "Wrote %s\n%!" name
+
+let measure_self n =
+  let hdr = init ()
+  and hdr' = init () in
+  for i = 1 to 1000 do
+    let ok = measure hdr (Hdr_histogram.record_value hdr') i in
+    assert ok
+  done;
+  dump_stats "self.histogram" hdr;
+  Hdr_histogram.close hdr;
+  Hdr_histogram.close hdr'
+
+let connect_hdr = init ()
+let sleep_hdr = init ()
 
 let run addr =
   let socket = Unix.socket Unix.PF_UNIX Unix.SOCK_DGRAM 0 in
   let finally () = Unix.close socket in
-  Fun.protect ~finally (measure socket addr)
-
-let print_header () =
-  List.iter (Printf.printf "\t%4.0f%%") percentiles ;
-  print_endline ""
+  Fun.protect ~finally
+  (fun () -> measure connect_hdr (Unix.connect socket) addr)
 
 let () =
-    measure_self 1000 100 ;
-  exit 0 ;
+  measure_self 100 ;
   let target = Sys.argv.(1) in
   let sockaddr = Unix.ADDR_UNIX target in
-  print_header () ;
-  for _ = 1 to 1000 do
-    for _ = 1 to 20 do
-      run sockaddr ; Unix.sleepf 0.047
-    done ;
-    dump_stats hdr
-  done
+  
+  (* todo: calc next sleep time, could be 0 if overrun, but count from actual schedule *)
+  Sys.catch_break true;
+  let finally () =
+    dump_stats "connect.histogram" connect_hdr;
+    dump_stats "sleepf.histogram" sleep_hdr
+  in
+
+  (* TODO: connect, send OPTIONS / , Connection:close,
+     read all
+
+     Measure latency for: first packet, entire response (read until EOF)
+
+     Also print 'testing' and a '.' after each attempt (fflush)
+
+     have a mode where we only send 1 char (cmdline flag), used to test virtual itimer effectiveness
+     on 100+16 test.
+  *)
+  
+  let run () = 
+    for _ = 1 to 100 do
+      for _ = 1 to 20 do
+        run sockaddr ;
+        Gc.minor ();
+        measure sleep_hdr Unix.sleepf 0.0047
+      done ;
+    done
+  in
+  Gc.full_major ();
+  Fun.protect ~finally run
