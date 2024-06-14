cc491ce6eb9f8dbfa69cf9cc7180a40ea354d75e
Edwin Török <edwin.torok@cloud.com>
2024-06-14 18:20:06 +0100

    twaks

--- tunelat/simple_measure.ml
+++ tunelat/simple_measure.ml
@@ -2,6 +2,8 @@
    For more detailed measurements see bench/ and bechamel
 *)
 
+let span_to_s span = Mtime.Span.to_float_ns span *. 1e-9
+
 let measure ?(n=1001) ?(inner=10) f counter =
    assert (n > 70);
    let measure_inner _ =
@@ -12,7 +14,7 @@ let measure ?(n=1001) ?(inner=10) f counter =
       done;
       let elapsed = Mtime_clock.count m in
       let v1 = Atomic.get counter in
-      (elapsed |> Mtime.Span.to_float_ns) *. 1e-9 /. (float @@ v1 - v0)
+      span_to_s elapsed /. (float @@ v1 - v0)
    in
    let measurements = Array.init n measure_inner in 
    Array.sort Float.compare measurements;
@@ -24,4 +26,15 @@ let measure ?(n=1001) ?(inner=10) f counter =
    and hi = n /. 2. +. 1. +. d |> Float.ceil |> Float.to_int in
    measurements.(lo - 1), median, measurements.(hi - 1)
 
+let rec measure_min f arg previous n =
+   if n > 0 then
+      let m = Mtime_clock.counter () in
+      Sys.opaque_identity (f arg);
+      let elapsed = Mtime_clock.count m in
+      measure_min f arg (Float.min previous @@ span_to_s elapsed) (n-1)
+   else previous
+
+let measure_min ?(n = 1001) f arg =
+   Sys.opaque_identity (f arg);
+   measure_min f arg Float.max_float n
 
--- tunelat/tunelat.ml
+++ tunelat/tunelat.ml
@@ -8,12 +8,47 @@ let worker () =
 
 let max_overhead_percentage = 0.5
 
-let () =
+let measure_yield_overhead () =
   let t = Thread.create worker () in
   let yield_lo, yield_overhead, yield_hi = Simple_measure.measure Operations.Yield.perform Operations.Yield.count in
   Printf.printf "Yield overhead: %.7fs,%.7fs,%.7fs\n" yield_lo yield_overhead yield_hi;
   Atomic.set stop true;
   Thread.join t;
+  yield_overhead
+
+let measure_caches () =
+  let caches = Cachesize.caches () in
+  let sizes = List.map (fun c -> c.Cachesize.size) caches in
+  let sizes = List.hd sizes * 2 :: sizes in
+  let linesize = List.fold_left Int.max 0 (List.map (fun c -> c.Cachesize.linesize) caches) in
+
+  let stride_size_bytes = linesize in
+  let timings =
+    sizes |> List.rev_map @@ fun size ->
+    let data = Operations.CycleRead.allocate ~stride_size_bytes size in
+    Gc.full_major ();
+    Simple_measure.measure_min ~n:10 Operations.CycleRead.read data /. (float size /. float stride_size_bytes)
+  in
+  let overheads =
+    (* not quite right, compare just to prev? *)
+(*    let op_overhead = List.hd timings in
+    List.map (fun t -> t -. op_overhead) (List.tl timings)  *)
+    let next = List.tl timings |> List.to_seq in
+    let prev = List.to_seq timings in
+    Seq.map2 (-.) next prev |> List.of_seq
+    
+  in
+  let overheads_time =
+    Seq.map2 (fun n t -> float (n / stride_size_bytes) *. t) (List.rev sizes |> List.to_seq) (List.to_seq overheads)
+    |> List.of_seq
+  in
+  List.iteri (fun i t -> Printf.printf "%d: %.9f/op\n" (i+1) t) timings;
+  List.iteri (fun i t -> Printf.printf "L%d cache miss: %.9f/op\n" (i+1) t) overheads;
+  List.iteri (fun i t -> Printf.printf "L%d cache miss: %.9fs\n" (i+1) t) overheads_time
+
+let () =
+  let yield_overhead = measure_yield_overhead () in
+  measure_caches ();
 
   let timeslice_min = yield_overhead /. (max_overhead_percentage /. 100.) in
   Printf.printf "timeslice: %.6fs\n" timeslice_min
