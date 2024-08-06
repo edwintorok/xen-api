edwintorok/xen-api

commit 1a9cc7660a839bee3135eb81979d616af55a6ddb

CP-20482: Create network with the specified bridge name
1. "managed" field is added to network
2. Add  specified bridge parameter to network.create API

Signed-off-by: Taoyong Ding <taoyong.ding@citrix.com>

ocaml/xapi/xapi_pif.ml
@@ -251,7 +251,7 @@ let find_or_create_network (bridge: string) (device: string) ~__context =
         ~current_operations:[] ~allowed_operations:[]
         ~name_label:(Helpers.choose_network_name_for_pif device)
         ~name_description:"" ~mTU:1500L
-        ~bridge ~other_config:[] ~blobs:[]
+        ~bridge ~managed:true ~other_config:[] ~blobs:[]
         ~tags:[] ~default_locking_mode:`unlocked ~assigned_ips:[]
     in
     net_ref

ocaml/idl/datamodel.ml
@@ -233,6 +233,12 @@ let get_product_releases in_product_since =
     | x::xs -> if x=in_product_since then "closed"::x::xs else go_through_release_order xs
   in go_through_release_order release_order
 
+let falcon_release =
+  { internal = get_product_releases rel_falcon
+  ; opensource=get_oss_releases None
+  ; internal_deprecated_since=None
+  }
+
 let dundee_plus_release =
   { internal = get_product_releases rel_dundee_plus
   ; opensource=get_oss_releases None
@@ -546,6 +552,8 @@ let _ =
     ~doc:"Only the local superuser can execute this operation" ();
 
   (* PIF/VIF/Network errors *)
+  error Api_errors.network_unmanaged [ "network" ]
+    ~doc:"The network is not managed by xapi." ();
   error Api_errors.network_already_connected ["network"; "connected PIF"]
     ~doc:"You tried to create a PIF, but the network you tried to attach it to is already attached to some other PIF, and so the creation failed." ();
   error Api_errors.cannot_destroy_system_network [ "network" ]
@@ -663,6 +671,9 @@ let _ =
     ~doc:"Operation cannot proceed while a tunnel exists on this interface." ();
   error Api_errors.bridge_not_available [ "bridge" ]
     ~doc:"Could not find bridge required by VM." ();
+  error Api_errors.bridge_name_exists [ "bridge" ]
+    ~doc:"The specified bridge already exists." ();
+
   (* VM specific errors *)
   error Api_errors.vm_is_protected [ "vm" ]
     ~doc:"This operation cannot be performed because the specified VM is protected by xHA" ();
@@ -5109,6 +5120,7 @@ let network_introduce_params first_rel =
     {param_type=Int; param_name="MTU"; param_doc=""; param_release=first_rel; param_default=None};
     {param_type=Map(String,String); param_name="other_config"; param_doc=""; param_release=first_rel; param_default=None};
     {param_type=String; param_name="bridge"; param_doc=""; param_release=first_rel; param_default=None};
+    {param_type=Bool; param_name="managed"; param_doc=""; param_release=falcon_release; param_default=None};
   ]
 
 (* network pool introduce is used to copy network records on pool join -- it's the network analogue of VDI/PIF.pool_introduce *)
@@ -5189,7 +5201,8 @@ let network =
           field ~qualifier:DynamicRO ~ty:(Set (Ref _pif)) "PIFs" "list of connected pifs";
           field ~qualifier:RW ~ty:Int ~default_value:(Some (VInt 1500L)) ~in_product_since:rel_midnight_ride "MTU" "MTU in octets";
           field ~writer_roles:_R_POOL_OP ~ty:(Map(String, String)) "other_config" "additional configuration" ~map_keys_roles:[("folder",(_R_VM_OP));("XenCenter.CustomFields.*",(_R_VM_OP));("XenCenterCreateInProgress",(_R_VM_OP))];
-          field ~in_oss_since:None ~qualifier:DynamicRO "bridge" "name of the bridge corresponding to this network on the local host";
+          field ~lifecycle:[Published, rel_rio, ""; Changed, rel_falcon, "Added to the constructor (network.create)"] ~in_oss_since:None ~qualifier:StaticRO  ~ty:String ~default_value:(Some (VString "")) "bridge" "name of the bridge corresponding to this network on the local host";
+          field ~lifecycle:[Published, rel_falcon, ""] ~qualifier:StaticRO ~ty:Bool ~default_value:(Some (VBool true)) "managed" "true if the bridge is managed by xapi";
           field ~qualifier:DynamicRO ~in_product_since:rel_orlando ~ty:(Map(String, Ref _blob)) ~default_value:(Some (VMap [])) "blobs" "Binary blobs associated with this network";
           field ~writer_roles:_R_VM_OP ~in_product_since:rel_orlando ~default_value:(Some (VSet [])) ~ty:(Set String) "tags" "user-specified tags for categorization purposes";
           field ~qualifier:DynamicRO ~in_product_since:rel_tampa ~default_value:(Some (VEnum "unlocked")) ~ty:network_default_locking_mode "default_locking_mode" "The network will use this value to determine the behaviour of all VIFs where locking_mode = default";

ocaml/perftest/createpool.ml
@@ -51,7 +51,7 @@ let initialise session_id template pool =
   let networks_to_create = pool.interfaces_per_host - pool.bonds in
   debug "Creating %d networks..." networks_to_create;
   let networks = Array.init networks_to_create (fun i ->
-      Client.Network.create ~rpc ~session_id ~name_label:(Printf.sprintf "perftestnet%d" i) ~name_description:"" ~mTU:1500L ~other_config:[oc_key,pool.key] ~tags:[])
+      Client.Network.create ~rpc ~session_id ~name_label:(Printf.sprintf "perftestnet%d" i) ~name_description:"" ~mTU:1500L ~other_config:[oc_key,pool.key] ~bridge:"" ~managed:true ~tags:[])
   in
 
   (* Set up the template - create the VIFs *)
@@ -380,7 +380,7 @@ let create_sdk_pool session_id sdkname pool_name key ipbase =
   let pifs = Client.PIF.get_all poolrpc poolses in
 
   let bondednets = Array.init pool.bonds (fun i ->
-      Client.Network.create poolrpc poolses (Printf.sprintf "Network associated with bond%d" i) "" 1500L [] [])
+      Client.Network.create poolrpc poolses (Printf.sprintf "Network associated with bond%d" i) "" 1500L [] "" true [])
   in
 
   let unused_nets = ref (List.setify (List.map (fun pif -> Client.PIF.get_network poolrpc poolses pif) pifs)) in

ocaml/xapi-consts/api_errors.ml
@@ -97,6 +97,7 @@ let device_detach_rejected = "DEVICE_DETACH_REJECTED"
 let operation_not_allowed = "OPERATION_NOT_ALLOWED"
 let operation_blocked = "OPERATION_BLOCKED"
 let network_already_connected = "NETWORK_ALREADY_CONNECTED"
+let network_unmanaged = "NETWORK_UNMANAGED"
 let cannot_destroy_system_network = "CANNOT_DESTROY_SYSTEM_NETWORK"
 let pif_is_physical = "PIF_IS_PHYSICAL"
 let pif_is_vlan = "PIF_IS_VLAN"
@@ -133,6 +134,7 @@ let transport_pif_not_configured = "TRANSPORT_PIF_NOT_CONFIGURED"
 let is_tunnel_access_pif = "IS_TUNNEL_ACCESS_PIF"
 let pif_tunnel_still_exists = "PIF_TUNNEL_STILL_EXISTS"
 let bridge_not_available = "BRIDGE_NOT_AVAILABLE"
+let bridge_name_exists = "BRIDGE_NAME_EXISTS"
 let vlan_tag_invalid = "VLAN_TAG_INVALID"
 let vm_bad_power_state = "VM_BAD_POWER_STATE"
 let vm_is_template = "VM_IS_TEMPLATE"

ocaml/xapi/cli_frontend.ml
@@ -2002,7 +2002,7 @@ add a mapping of 'path' -> '/tmp', the command line should contain the argument 
     "network-create",
     {
       reqd=["name-label"];
-      optn=["name-description"; "MTU"];
+      optn=["name-description"; "MTU"; "bridge"; "managed"];
       help="Create a new network.";
       implementation=No_fd Cli_operations.net_create;
       flags=[];

ocaml/xapi/cli_operations.ml
@@ -1616,7 +1616,9 @@ let net_create printer rpc session_id params =
   let network = List.assoc "name-label" params in
   let descr = List.assoc_default "name-description" params "" in
   let mtu = if List.mem_assoc "MTU" params then Int64.of_string (List.assoc "MTU" params) else 1500L in
-  let net = Client.Network.create rpc session_id network descr mtu [] [] in
+  let bridge = List.assoc_default "bridge" params "" in
+  let managed = get_bool_param params ~default:true "managed" in
+  let net = Client.Network.create rpc session_id network descr mtu [] bridge managed [] in
   let uuid = Client.Network.get_uuid rpc session_id net in
   printer (Cli_printer.PList [uuid])
 

ocaml/xapi/cli_operations_geneva.ml
@@ -424,7 +424,7 @@ let host_vbridge_add printer rpc session_id params =
   let autoadd = List.assoc "auto-vm-add" params in
   let desc = try List.assoc "vbridge-description" params with _ -> "" in
   ignore(Client.Network.create rpc session_id name desc 1500L
-           (if autoadd="true" then [("auto_add_to_VM",autoadd);("geneva-name",name)] else [("geneva-name",name)]) [])
+           (if autoadd="true" then [("auto_add_to_VM",autoadd);("geneva-name",name)] else [("geneva-name",name)]) "" true [])
 
 let host_vbridge_remove printer rpc session_id params =
   let name = List.assoc "vbridge-name" params in

ocaml/xapi/create_networks.ml
@@ -39,7 +39,7 @@ let create_guest_installer_network ~__context =
     (* The new "host internal management network" is created with both other_config keys *)
     let h' = Xapi_network.create ~__context ~name_label:internal_management_network_name
         ~name_description:internal_management_network_desc ~mTU:1500L
-        ~other_config:internal_management_network_oc ~tags:[] in
+        ~other_config:internal_management_network_oc ~bridge:"" ~managed:true ~tags:[] in
     Db.Network.set_bridge ~__context ~self:h' ~value:internal_management_bridge;
     debug "Created new host internal management network: %s" (Ref.string_of h')
 

commit 3bbf5588cc45b99e0541d9eb3658b78f71384077

CA-396743: make Network.managed reflect PIF.managed
This will make it easier to filter out these networks in test code.
A PIF is unmanaged when it is a boot from SAN interface for example,
as returned by the 'bfs-interfaces' script.

Certain operations are not valid on such interfaces, e.g. you cannot use them to export NBD devices.

Fixes: 1a9cc7660a ("CP-20482: Create network with the specified bridge name")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xapi/xapi_pif.ml
@@ -347,7 +347,8 @@ let assert_fcoe_not_in_use ~__context ~self =
              ()
      )
 
-let find_or_create_network (bridge : string) (device : string) ~__context =
+let find_or_create_network (bridge : string) (device : string) ~managed
+    ~__context =
   let nets =
     Db.Network.get_refs_where ~__context
       ~expr:(Eq (Field "bridge", Literal bridge))
@@ -362,7 +363,7 @@ let find_or_create_network (bridge : string) (device : string) ~__context =
         Db.Network.create ~__context ~ref:net_ref ~uuid:net_uuid
           ~current_operations:[] ~allowed_operations:[]
           ~name_label:(Helpers.choose_network_name_for_pif device)
-          ~name_description:"" ~mTU:1500L ~purpose:[] ~bridge ~managed:true
+          ~name_description:"" ~mTU:1500L ~purpose:[] ~bridge ~managed
           ~other_config:[] ~blobs:[] ~tags:[] ~default_locking_mode:`unlocked
           ~assigned_ips:[]
       in
@@ -463,7 +464,7 @@ let introduce_internal ?network ?(physical = true) ~t:_ ~__context ~host ~mAC
   let net_ref =
     match network with
     | None ->
-        find_or_create_network bridge device ~__context
+        find_or_create_network bridge device ~managed ~__context
     | Some x ->
         x
   in