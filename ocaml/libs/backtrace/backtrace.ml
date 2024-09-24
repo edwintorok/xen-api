f52132944e0491402b02cebe9a962e3e23ea6a90
Edwin Török <edwin.torok@cloud.com>
2024-09-24 15:31:01 +0100

    CP-49134: simplify Backtrace module
    Remove all the imperative stacktrace handling and per-thread tables.
    Not needed, we can attach the raw backtrace to the exception type instead.
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/libs/backtrace/backtrace.ml
+++ ocaml/libs/backtrace/backtrace.ml
@@ -13,26 +13,7 @@
  *)
 open Sexplib0.Sexp_conv
 
-let my_name = ref (Filename.basename Sys.argv.(0))
-
-let set_my_name x = my_name := x
-
-module Mutex = struct
-  include Mutex
-
-  (** execute the function f with the mutex hold *)
-  let execute lock f =
-    Mutex.lock lock ;
-    let r = try f () with exn -> Mutex.unlock lock ; raise exn in
-    Mutex.unlock lock ; r
-end
-
-let rec split_c c str =
-  try
-    let i = String.index str c in
-    String.sub str 0 i
-    :: split_c c (String.sub str (i + 1) (String.length str - i - 1))
-  with Not_found -> [str]
+let my_name = Filename.basename Sys.argv.(0)
 
 type frame = {
     process: string
@@ -82,18 +63,6 @@ let to_string_hum xs =
   in
   loop true 1 xs
 
-type table = {
-    backtraces: t array
-  ; exn_to_backtrace: exn Weak.t
-  ; mutable producer: int (* free running counter *)
-  ; m: Mutex.t
-}
-
-(* Increasing this makes 'find_all' slower and increases the amount of
-   memory needed. Since we have a table per thread a small number should
-   be enough. *)
-let max_backtraces = 100
-
 let frame_of_slot process slot =
   let open Printexc in
   match Slot.location slot with
@@ -118,7 +87,7 @@ let fold_dedup_frames revlst item =
 let frames_of_slots interop slots =
   slots
   |> Array.to_seq
-  |> Seq.filter_map (frame_of_slot !my_name)
+  |> Seq.filter_map (frame_of_slot my_name)
   |> Seq.fold_left fold_dedup_frames (List.rev interop)
   |> List.rev
 
@@ -131,132 +100,6 @@ let of_raw bt = of_raw_and_interop bt empty
 
 let get_backtrace_411 () = Printexc.get_raw_backtrace () |> of_raw
 
-let make () =
-  let backtraces = Array.make max_backtraces [] in
-  let exn_to_backtrace = Weak.create max_backtraces in
-  let producer = 0 in
-  (* free running *)
-  let m = Mutex.create () in
-  {backtraces; exn_to_backtrace; producer; m}
-
-let add t exn bt =
-  Mutex.execute t.m (fun () ->
-      let slot = t.producer mod max_backtraces in
-      t.producer <- t.producer + 1 ;
-      Weak.set t.exn_to_backtrace slot (Some exn) ;
-      t.backtraces.(slot) <- bt
-  )
-
-let is_important t exn =
-  let bt = get_backtrace_411 () in
-  (* Deliberately clear the backtrace buffer *)
-  (try raise Not_found with Not_found -> ()) ;
-  add t exn bt
-
-(* fold over the slots matching exn *)
-let fold t exn f initial =
-  let rec loop acc from =
-    if from < 0 || t.producer - from > max_backtraces then
-      acc
-    else
-      let slot = from mod max_backtraces in
-      match Weak.get t.exn_to_backtrace slot with
-      | Some exn' when exn' == exn ->
-          loop (f acc slot) (from - 1)
-      | _ ->
-          loop acc (from - 1)
-  in
-  loop initial (t.producer - 1)
-
-let remove_dups xs =
-  List.fold_left
-    (fun (last, acc) item -> (item, if last = item then acc else item :: acc))
-    ([], []) (List.rev xs)
-  |> snd
-
-(*
-  |> List.rev
-*)
-let get t exn =
-  fold t exn (fun acc slot -> t.backtraces.(slot) :: acc) []
-  |> remove_dups
-  |> List.concat
-
-let remove t exn =
-  fold t exn
-    (fun acc slot ->
-      let bt = t.backtraces.(slot) in
-      Weak.set t.exn_to_backtrace slot None ;
-      t.backtraces.(slot) <- [] ;
-      bt :: acc
-    )
-    []
-  |> remove_dups
-  |> List.concat
-
-let per_thread_backtraces = Hashtbl.create 37
-
-let per_thread_backtraces_m = Mutex.create ()
-
-let with_lock f x =
-  Mutex.lock per_thread_backtraces_m ;
-  try
-    let result = f x in
-    Mutex.unlock per_thread_backtraces_m ;
-    result
-  with e ->
-    Mutex.unlock per_thread_backtraces_m ;
-    raise e
-
-let with_backtraces f =
-  let id = Thread.(id (self ())) in
-  let tbl =
-    with_lock
-      (fun () ->
-        let tbl =
-          if Hashtbl.mem per_thread_backtraces id then
-            Hashtbl.find per_thread_backtraces id
-          else
-            make ()
-        in
-        (* If we nest these functions we add multiple bindings
-           to the same mutable table which is ok *)
-        Hashtbl.add per_thread_backtraces id tbl ;
-        tbl
-      )
-      ()
-  in
-  try
-    let result = f () in
-    with_lock (Hashtbl.remove per_thread_backtraces) id ;
-    `Ok result
-  with e ->
-    let bt = get tbl e in
-    with_lock (Hashtbl.remove per_thread_backtraces) id ;
-    `Error (e, bt)
-
-let with_table f default =
-  let id = Thread.(id (self ())) in
-  match
-    with_lock
-      (fun () ->
-        if Hashtbl.mem per_thread_backtraces id then
-          Some (Hashtbl.find per_thread_backtraces id)
-        else
-          None
-      )
-      ()
-  with
-  | None ->
-      default ()
-  | Some tbl ->
-      f tbl
-
-let is_important exn =
-  with_table (fun tbl -> is_important tbl exn) (fun () -> ())
-
-let add exn bt = with_table (fun tbl -> add tbl exn bt) (fun () -> ())
-
 let make process filename line =
   {
     process
@@ -268,24 +111,6 @@ let make process filename line =
   ; is_inline= None
   ; is_raise= None
   }
-
-let warning () =
-  [
-    make !my_name
-      (Printf.sprintf
-         "(Thread %d has no backtrace table. Was with_backtraces called?"
-         Thread.(id (self ()))
-      )
-      0
-  ]
-
-let remove exn = with_table (fun tbl -> remove tbl exn) warning
-
-let get exn = with_table (fun tbl -> get tbl exn) warning
-
-let reraise old newexn =
-  add newexn (remove old) ;
-  raise newexn
 
 module Interop = struct
   (* This matches xapi.py:exception *)
--- ocaml/libs/backtrace/backtrace.mli
+++ ocaml/libs/backtrace/backtrace.mli
@@ -56,45 +56,6 @@ val try_with : (unit -> 'a) -> (exn -> Printexc.raw_backtrace -> 'a) -> 'a
     ]}
  *)
 
-val with_backtraces : (unit -> 'a) -> [`Ok of 'a | `Error of exn * t]
-(** Allow backtraces to be recorded for this thread. All new threads
-    must be wrapped in this for the backtrace tracking to work.
-    It is acceptable to nest these wrappers; it will not affect the
-    backtrace recording behaviour. *)
-
-val is_important : exn -> unit
-(** Declare that the backtrace is important for debugging and should be
-    permanently associated with the exception. Call this function in
-    an exception handler where you might need to re-raise the same
-    exception at the end after performing some cleanup, which could
-    clear the current backtrace buffer.*)
-
-val get : exn -> t
-(** Get a copy of the backtrace associated with [exn] *)
-
-val add : exn -> t -> unit
-(** Associate additional backtrace with an exception. This allows
-    you to combine a backtrace from another process with your current
-    backtrace. *)
-
-val reraise : exn -> exn -> 'a
-(** [reraise old new] associates the backtrace of [old] with [new]
-    and throws [new]. Use this if you need to 'launder' an exception
-    e.g. you may want to catch Not_found and throw a more descriptive
-    exception instead without losing the backtrace. *)
-
-val remove : exn -> t
-(** Get a backtrace associated with [exn] and remove it from the tables.
-    Use this when you want to print/log or otherwise record the final
-    backtrace. *)
-
-(** {2 Administrivia} *)
-
-val set_my_name : string -> unit
-(** Every backtrace line will include a name for this process. By default it
-    will be the executable name, but it could also include the process ID
-    and host. *)
-
 (** {2 Interop with other languages}
     This allows backtraces from other languages (e.g. python) to be converted
     into OCaml-style backtraces. *)
