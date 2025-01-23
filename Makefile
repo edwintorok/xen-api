edwintorok/xen-api

commit e6afe15bfde080bd204c36ccb9c2773002166f98

Removed erroneously ported recipe.
Signed-off-by: Konstantina Chremmou <Konstantina.Chremmou@cloud.com>

Makefile
@@ -117,15 +117,10 @@ sdk:
 	sh ocaml/sdk-gen/windows-line-endings.sh $(XAPISDK)/csharp
 	sh ocaml/sdk-gen/windows-line-endings.sh $(XAPISDK)/powershell
 
-.PHONY: sdk-build-c sdk sdksanity
+.PHONY: sdk-build-c
 
 sdk-build-c: sdk
-	cd _build/install/default/xapi/sdk/c && make -j $(JOBS)
-
-# workaround for no .resx generation, just for compilation testing
-sdksanity: sdk
-	sed -i 's/FriendlyErrorNames.ResourceManager/null/g' ./_build/install/default/xapi/sdk/csharp/src/Failure.cs
-	cd _build/install/default/xapi/sdk/csharp/src && dotnet add package Newtonsoft.Json && dotnet build -f netstandard2.0
+	cd _build/install/default/xapi/sdk/c && make clean && make -j $(JOBS)
 
 .PHONY: sdk-build-java
 

commit 9ccc6348cf5d7c888822d6782b6763cabaffe940

Makefile: add sdksanity rule for testing on Linux
To allow compile-testing the C# code on Linux reintroduce the
'sdksanity' rule which got dropped accidentally.
Also run the tests.

Fixes: e6afe15bf ("Removed erroneously ported recipe.")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

Makefile
@@ -83,6 +83,11 @@ doc:
 sdk:
 	dune build --profile=$(PROFILE)  @sdkgen xapi-sdk.install @ocaml/sdk-gen/install
 
+# workaround for no .resx generation, just for compilation testing
+sdksanity: sdk
+	cd _build/install/default/share/csharp/src && dotnet add package Newtonsoft.Json && dotnet build -f netstandard2.0
+	cd _build/install/default/share/csharp && dotnet test XenServerTest -p:DefineConstants=BUILD_FOR_TEST
+
 .PHONY: sdk-build-c
 
 sdk-build-c: sdk