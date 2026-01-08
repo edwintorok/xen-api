d2e1154a6b45c543416cff97535d88dd2686d61e
Edwin Török <edwin.torok@cloud.com>
2026-01-08 23:21:58 +0000

    wip

--- ocaml/xapi-client/util/xapi_client_util.ml
+++ ocaml/xapi-client/util/xapi_client_util.ml
@@ -33,9 +33,44 @@ let raise_for_task_exn ~rpc ~session_id remote_task =
 
 let rpc_of_task = function "" -> Rpc.Null | s -> Xmlrpc.of_string s
 
+let on_task f task =
+  (* Task may have been GCed meanwhile, or changed state on its own,
+     ignore exceptions when canceling and destroying *)
+  try f task with Api_errors.Server_error (_, _) -> ()
+
 let result_of_task ~rpc ~session_id self =
-  match raise_for_task_exn ~rpc ~session_id self with
-  | exception e ->
-      Error (e, Printexc.get_raw_backtrace ())
-  | () ->
-      Client.Task.get_result ~rpc ~session_id ~self |> rpc_of_task |> Result.ok
+  let call f = f ~rpc ~session_id in
+  let outcome =
+    try
+      call @@ raise_for_task_exn self ;
+      call @@ Client.Task.get_result ~self |> rpc_of_task |> Result.ok
+    with e -> Error (e, Printexc.get_raw_backtrace ())
+  in
+  let () = self |> on_task @@ fun self -> call @@ Client.Task.destroy ~self in
+  outcome
+
+let results_of_tasks ~rpc ~session_id tasks =
+  let call f = f ~rpc ~session_id in
+  let callback _ self =
+    let () =
+      if call @@ Client.Task.get_status ~self = `failure then
+        tasks
+        |> List.filter (( <> ) self)
+        |> List.iter @@ on_task @@ fun task -> call @@ Client.Task.cancel ~task
+    in
+    []
+  in
+  call @@ Tasks.wait_for_all_with_callback ~tasks ~callback ;
+  tasks |> List.map (call @@ result_of_task)
+
+let map_async ~rpc ~session_id f lst =
+  let call f = f ~rpc ~session_id in
+  lst |> List.map (call @@ f) |> call @@ results_of_tasks
+
+let with_objects ~rpc ~session_id input create destroy f =
+  let call f = f ~rpc ~session_id in
+  let objects = input |> call @@ map_async create in
+  (* TODO: ensure exceptions don't escape, wrap *)
+  let outcomes = List.map f objects in
+  let (_ : _ list) = objects |> call @@ map_async destroy in
+  outcomes
--- ocaml/xapi-client/util/xapi_client_util.mli
+++ ocaml/xapi-client/util/xapi_client_util.mli
@@ -7,8 +7,23 @@ val raise_for_task_exn :
     @raises Api_errors.server_error if the task failed, together with a
     server-side backtrace *)
 
-val result_of_task:
+val result_of_task :
      rpc:(Rpc.call -> Rpc.response)
   -> session_id:API.ref_session
   -> API.ref_task
   -> (Rpc.t, exn * Printexc.raw_backtrace) Result.t
+(** [result_of_task ~rpc ~session_id task] returns the result of [task], or a
+    an exception and a backtrace.
+    Consumes [task] by destroying it at the end.
+*)
+
+val results_of_tasks :
+     rpc:(Rpc.call -> Rpc.response)
+  -> session_id:API.ref_session
+  -> API.ref_task list
+  -> (Rpc.t, exn * Printexc.raw_backtrace) Result.t list
+(** [results_of_tasks ~rpc ~session_id tasks] returns the results of [tasks], or a
+    an exception and a backtrace.
+    Consumes [tasks] by destroying them at the end.
+    If any task fails then it cancels all other pending tasks.
+*)
